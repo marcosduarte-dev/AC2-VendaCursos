a62d90185f6a93621053c147bebafd1b69b270e7
Marcos Duarte <pe.marcos30@gmail.com>
2024-11-13 13:44:51 -0300

    Adicionando jacoco

--- src/main/java/com/Venda/VendaCursos/VendaCursosApplication.java
+++ src/main/java/com/Venda/VendaCursos/VendaCursosApplication.java
@@ -2,10 +2,12 @@ package com.Venda.VendaCursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class VendaCursosApplication {
 
 	public static void main(String[] args) {
